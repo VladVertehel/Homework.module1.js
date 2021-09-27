35e5707e9336b4066871f25e6477a76de9f7cf08
Yabluko <83119163+VladVertehel@users.noreply.github.com>
2021-09-27 18:58:34 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+// task 1
+let a = [1, 2, 3];
+a.push(4, 5, 6);
+console.log(a);
+
+// task 2
+a.unshift(4, 5, 6);
+console.log(a);
+
+// task 3
+let b = ['js', 'css', 'jq'];
+console.log(b[0]);
+
+// task 4
+console.log(b[b.length - 1]);
+
+// task 5
+let c = [1, 2, 3, 4, 5];
+let newC = c.slice(0, 3);
+console.log(newC);
+
+// task 6
+let d = [1, 2, 3, 4, 5];
+let newD = d.slice(3);
+console.log(newD);
